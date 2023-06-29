14e316f93710d673a0545bbbcbea3e0bddc49b97
Joubert RedRat <eu+git@redrat.com.br>
2023-06-29 17:00:27 +0100

    add missing dto test

---
+++ tests/Dto/CreateFansubTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Dto;
+
+use App\Dto\CreateFansub;
+use App\Exception\Dto\CreateFansub\InvalidNameException;
+use PHPUnit\Framework\TestCase;
+
+class CreateFansubTest extends TestCase
+{
+    public function testCreateFansub(): void
+    {
+        $nameExpected = 'Fansub';
+        $createFansub = new CreateFansub(name: $nameExpected);
+
+        self::assertEquals($nameExpected, $createFansub->name);
+    }
+
+    public function testCreateTagWithInvalidName(): void
+    {
+        $this->expectException(InvalidNameException::class);
+        new CreateFansub(name: '');
+    }
+}
