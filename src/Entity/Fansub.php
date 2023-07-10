61cfba03b8f852621b90199dadc4bb4532608ebe
Joubert RedRat <eu+git@redrat.com.br>
2023-07-10 20:11:22 +0100

    finish create and list fansubs

--- src/Entity/Fansub.php
+++ src/Entity/Fansub.php
@@ -10,6 +10,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: FansubRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Fansub
 {
     use Timestampable;
