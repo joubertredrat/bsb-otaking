b961cde928b45dbbb7ea7903b81d61b2c6f6e1c6
Joubert RedRat <eu+git@redrat.com.br>
2023-07-12 17:04:23 +0100

    add missing tests

---
+++ tests/EventListener/ExceptionListenerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\EventListener;
+
+use App\EventListener\ExceptionListener;
+use App\Exception\Http\Request\AbstractJsonRequest\InvalidJsonRequest;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class ExceptionListenerTest extends TestCase
+{
+    public function testOnKernelExceptionWithInvalidJsonRequestException(): void
+    {
+        $dispatcher = new EventDispatcher();
+        $listener = new ExceptionListener();
+        $dispatcher->addListener('onKernelException', [$listener, 'onKernelException']);
+
+        $kernel = Mockery::mock(HttpKernelInterface::class);
+        $request = Request::createFromGlobals();
+        $event = new ExceptionEvent(
+            kernel: $kernel,
+            request: $request,
+            requestType: HttpKernelInterface::MAIN_REQUEST,
+            e: InvalidJsonRequest::dispatch(['foo' => 'bar']),
+        );
+
+        $dispatcher->dispatch($event, 'onKernelException');
+        $this->assertInstanceOf(JsonResponse::class, $event->getResponse());
+    }
+}
